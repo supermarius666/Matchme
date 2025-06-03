65c56821a315b3b9446f7fa99c2bdc26ebed1e9e
Francesco Lucarini (Apothiphis_Z) <apothiphis.z@gmail.com>
2025-06-03 21:37:45 +0200

    Fatto il db per Eventi e Posti, rese dinamiche le pagine, messo slider per eventi, reso responsive

--- back-end/apps/events/models.py
+++ back-end/apps/events/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='events_pics/')
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='places_pics/')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    description_intro = models.TextField(blank=True, null=True)
+    description_couples = models.TextField(blank=True, null=True)
+    description_history = models.TextField(blank=True, null=True)
+    description_outro = models.TextField(blank=True, null=True)
+    link1 = models.URLField(blank=True, null=True)
+    link2 = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name.replace('_', ' ').title()
+    
--- back-end/apps/events/urls.py
+++ back-end/apps/events/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.events_view, name="events"),
-    path('<str:placeName>', views.place_view, name="place"),
+    path('', views.events_main_view, name="events"),
+    path('<str:placeName>', views.place_event_view, name="place_event"),
 ]
